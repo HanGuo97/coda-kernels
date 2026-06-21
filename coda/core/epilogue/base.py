5e097f27fb5ead7b14e4d99db351dee728776860
Han Guo <alex.guohan1106@gmail.com>
2026-06-21 13:44:06 -0700

    updated

--- coda/core/epilogue/base.py
+++ coda/core/epilogue/base.py
@@ -1,8 +1,11 @@
 import cutlass
 import cutlass.cute as cute
+from dataclasses import MISSING
+from typing import Callable, Iterable
 
 from quack.gemm_sm90 import GemmSm90
 from quack.cute_dsl_utils import mlir_namedtuple, ParamsBase
+from quack.epi_ops import Scalar, RowVecLoad, ColVecLoad, TileStore, TileLoad, EpiOp
 
 
 class Epilogue(object):
@@ -65,3 +68,47 @@ class _Composite(Epilogue):
 
 def compose(epilogues: Iterable["Epilogue"]) -> "Epilogue":
     return _Composite(list(epilogues))
+
+
+def _arg_field(op: EpiOp) -> tuple:
+    if isinstance(op, Scalar):
+        if op.dtype is None:
+            # if `dtype` is None, quack defaults to FP32
+            # https://github.com/Dao-AILab/quack/blob/v0.5.2/quack/epi_ops.py#L281
+            dtype = cute.Float32
+        else:
+            dtype = op.dtype
+        return (op.name, dtype | cute.Tensor | None, None)
+    if isinstance(op, (RowVecLoad, ColVecLoad, TileLoad)):
+        return (op.name, cute.Tensor | None, None)
+    if isinstance(op, TileStore):
+        return (op.name, cute.Tensor, MISSING)
+    raise TypeError(f"unknown op {op!r}")
+
+
+def _ops_compatible(op_a: EpiOp, op_b: EpiOp) -> bool:
+    return all([
+        type(op_a) is type(op_b),
+        _arg_field(op_a) == _arg_field(op_b),
+        getattr(op_a, "epi_tile_fn", None) is getattr(op_b, "epi_tile_fn", None),
+    ])
+
+
+def _normalize(ops: Iterable[EpiOp]) -> tuple[EpiOp, ...]:
+    ops_dict = {}
+    ops_normalized = []
+    for op in ops:
+        op_prev = ops_dict.get(op.name)
+        if op_prev is None:
+            # new op
+            ops_dict[op.name] = op
+            ops_normalized.append(op)
+        elif not _ops_compatible(op, op_prev):
+            # duplicate but imcompatible op
+            raise ValueError
+
+    # we only support one output for now
+    if sum(isinstance(op, TileStore) for op in ops_normalized) > 1:
+        raise NotImplementedError
+
+    return tuple(ops_normalized)
