22e39f2ff8f26377a9b8effaeb98b6b85ffc9703
Han Guo <alex.guohan1106@gmail.com>
2026-06-21 15:17:35 -0700

    updated

--- coda/core/epilogue/base.py
+++ coda/core/epilogue/base.py
@@ -1,11 +1,14 @@
 import cutlass
 import cutlass.cute as cute
 from dataclasses import MISSING
-from typing import Callable, Iterable
+from typing import Iterable, NamedTuple
 
 from quack.gemm_sm90 import GemmSm90
 from quack.cute_dsl_utils import mlir_namedtuple, ParamsBase
 from quack.epi_ops import Scalar, RowVecLoad, ColVecLoad, TileStore, TileLoad, EpiOp
+from quack.epi_composable import ComposableEpiMixin
+from quack.gemm_act import GemmActMixin
+from quack.rounding import RoundingMode
 
 
 class Epilogue(object):
@@ -104,7 +107,7 @@ def _normalize(ops: Iterable[EpiOp]) -> tuple[EpiOp, ...]:
             ops_dict[op.name] = op
             ops_normalized.append(op)
         elif not _ops_compatible(op, op_prev):
-            # duplicate but imcompatible op
+            # duplicate but incompatible op
             raise ValueError
 
     # we only support one output for now
@@ -112,3 +115,65 @@ def _normalize(ops: Iterable[EpiOp]) -> tuple[EpiOp, ...]:
         raise NotImplementedError
 
     return tuple(ops_normalized)
+
+
+def _make_args(fields: list[tuple]) -> type:
+    required = []
+    optional = []
+    optional_vals = []
+    for name, annotation, default in fields:
+        if default is MISSING:
+            required.append((name, annotation))
+        else:
+            optional.append((name, annotation))
+            optional_vals.append(default)
+    cls = NamedTuple("EpilogueArguments", required + optional)
+    # `__defaults__` bind to trailing parameters
+    cls.__new__.__defaults__ = tuple(optional_vals)
+    return mlir_namedtuple(cls)
+
+
+def _lower(epilogue: Epilogue, name: str, gemm_cls: type) -> type:
+    ops = _normalize(epilogue.declares())
+    has_aux = any(isinstance(op, TileStore) for op in ops)
+    fields = [_arg_field(op) for op in ops]
+    fields.append(("rounding_mode", cutlass.Constexpr[int], RoundingMode.RN))
+
+    class EpiMixin(ComposableEpiMixin):
+        _epi_ops = ops
+        _has_aux = has_aux
+        _epilogue = epilogue
+        EpilogueArguments = _make_args(fields)
+
+        def epi_to_underlying_arguments(self, args: EpilogueArguments, *, loc=None, ip=None):
+            self.rounding_mode = args.rounding_mode
+            if self._has_aux:
+                self.aux_out_dtype = args.mAuxOut.element_type
+                self.aux_out_layout = cutlass.utils.LayoutEnum.from_tensor(args.mAuxOut)
+                self.cta_tile_shape_aux_out_mn = self.cta_tile_shape_mnk[:2]
+            return self.EpilogueParams(**self._epi_ops_to_params_dict(args))
+
+        @cute.jit
+        def epi_visit_subtile(
+            self,
+            params: ParamsBase,
+            epi_loop_tensors: dict,
+            tRS_rD: cute.Tensor,
+            tRS_rC: cute.Tensor | None,
+        ) -> tuple[cute.Tensor, ...]:
+            return self._epilogue.visit(
+                gemm=self,
+                params=params,
+                epi_loop_tensors=epi_loop_tensors,
+                tRS_rD=tRS_rD,
+                tRS_rC=tRS_rC,
+            )
+
+    if has_aux:
+        # `GemmActMixin` handles the auxiliary stores
+        bases = (EpiMixin, GemmActMixin, gemm_cls)
+    else:
+        bases = (EpiMixin, gemm_cls)
+
+    # https://github.com/Dao-AILab/quack/blob/v0.5.2/quack/gemm_act.py#L295
+    return type(name, bases, {})
