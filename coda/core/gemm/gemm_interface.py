d224a537b3c2de3dd058355e653ccc57cdd48888
Han Guo <alex.guohan1106@gmail.com>
2026-06-25 10:15:34 -0700

    interface cleanup

--- coda/core/gemm/gemm_interface.py
+++ coda/core/gemm/gemm_interface.py
@@ -78,31 +78,42 @@ def gemm_epilogue(
     tile_N: int,
     cluster_M: int,
     cluster_N: int,
+    cluster_K: int,
     tile_K: int | None,
     pingpong: bool,
     persistent: bool,
     is_dynamic_persistent: bool,
     max_swizzle_size: int,
+    batch_idx_permute: torch.Tensor | None,
+    add_to_output: bool,
+    rounding_mode: RoundingMode,
 ) -> None:
-
-    A_p = perm3d_single(A, varlen_m=False)
-    B_p = perm3d_single(B, varlen_m=False)
-    D_p = perm3d_single(D, varlen_m=False)
-    C_p = perm3d_single(C, varlen_m=False)
 
     device_capacity = get_device_capacity(A.device)
     assert device_capacity[0] in [8, 9, 10, 11, 12], (
         "Only SM8x, SM90, SM100, SM110, and SM120 are supported"
     )
     if rounding_mode == RoundingMode.RS:
-        assert device_capacity[0] == 10, "Stochastic rounding (RoundingMode.RS) requires SM100"
-
-    if is_dynamic_persistent and device_capacity[0] == 9:
+        raise NotImplementedError
+    if is_dynamic_persistent and device_capacity[0] <= 9:
         assert tile_count_semaphore is not None, (
-            "Dynamic persistent tile scheduler in SM90 requires a semaphore in GMEM"
+            "Dynamic persistent tile scheduler for SM8x and SM90 requires a semaphore in GMEM"
         )
+    if device_capacity[0] == 8:
+        if add_to_output:
+            C = D
+            add_to_output = False
 
-    compiled_fn = _compile(
+    A_p, B_p, D_p, C_p = perm3d(
+        A=A,
+        B=B,
+        D=D,
+        C=C,
+        varlen_m=False,
+        varlen_k=False,
+    )
+
+    compiled_fn = _compile_gemm(
         a_dtype=torch2cute_dtype_map[A.dtype],
         b_dtype=torch2cute_dtype_map[B.dtype],
         d_dtype=torch2cute_dtype_map[D.dtype] if D is not None else None,
@@ -111,19 +122,33 @@ def gemm_epilogue(
         b_major=get_major(B_p, "n", "k"),
         d_major=get_major(D_p, "m", "n") if D_p is not None else None,
         c_major=get_major(C_p, "m", "n") if C_p is not None else None,
-        tile_M=tile_M,
-        tile_N=tile_N,
-        tile_K=tile_K,
-        cluster_M=cluster_M,
-        cluster_N=cluster_N,
+        tile_shape_mnk=(tile_M, tile_N) if tile_K is None else (tile_M, tile_N, tile_K),
+        cluster_shape_mnk=(cluster_M, cluster_N, cluster_K),
         pingpong=pingpong,
         persistent=persistent,
         is_dynamic_persistent=is_dynamic_persistent,
+        add_to_output=add_to_output,
+        concat_layout=None,
+        varlen_m=False,
+        varlen_k=False,
+        gather_A=False,
+        use_tma_gather=False,
+        has_batch_idx_permute=batch_idx_permute is not None,
         device_capacity=device_capacity,
+        rounding_mode=rounding_mode,
+        sr_seed_mode=None,
+        num_warps=None,
         gemm_cls_name=GemmCls.__name__,
     )
 
-    max_active_clusters = get_max_active_clusters(cluster_M * cluster_N) if persistent else 0
+    cluster_size = cluster_M * cluster_N * cluster_K
+    max_active_clusters = (
+        get_max_active_clusters(
+            cluster_size=cluster_size,
+            device_capacity=device_capacity,
+        )
+        if persistent else 0
+    )
 
     epi_args = GemmCls.EpilogueArguments(
         add_to_output=None,
@@ -131,12 +156,18 @@ def gemm_epilogue(
         sr_seed=None,
     )
     scheduler_args = make_scheduler_args(
-        max_active_clusters,
-        max_swizzle_size,
-        tile_count_semaphore,
+        max_active_clusters=max_active_clusters,
+        max_swizzle_size=max_swizzle_size,
+        tile_count_semaphore=tile_count_semaphore,
+        batch_idx_permute=batch_idx_permute,
+    )
+    varlen_args = make_varlen_args(
+        cu_seqlens_m=None,
+        cu_seqlens_k=None,
+        A_idx=None,
     )
 
     if device_capacity[0] in [10, 11]:
-        compiled_fn(A_p, B_p, D_p, C_p, epi_args, scheduler_args, None, None, None)
+        compiled_fn(A_p, B_p, D_p, C_p, epi_args, scheduler_args, varlen_args, None, None)
     else:
-        compiled_fn(A_p, B_p, D_p, C_p, epi_args, scheduler_args, None)
+        compiled_fn(A_p, B_p, D_p, C_p, epi_args, scheduler_args, varlen_args)
