688d9cafd082ad9fd310af143e89ac99f9bf2d9e
Han Guo <alex.guohan1106@gmail.com>
2026-07-05 23:04:38 -0700

    added RoPE

--- coda/core/epilogue/activation.py
+++ coda/core/epilogue/activation.py
@@ -3,7 +3,7 @@ import cutlass.cute as cute
 from typing import Callable
 
 from quack.cute_dsl_utils import ParamsBase
-from quack.epi_ops import EpiOp, TileStore
+from quack.epi_ops import EpiOp, ColVecLoad, RowVecLoad, TileStore
 from quack.gemm_act import GemmActMixin, GemmGatedMixin, _gated_epi_tile_fn
 from quack.gemm_sm90 import GemmSm90
 
@@ -102,3 +102,42 @@ class Gated(Epilogue):
         else:
             tRS_rAuxOut = tRS_rD
         return (tRS_rAuxOut,)
+
+
+class RoPE(Epilogue):
+
+    def __init__(self, pos_idx_name: str | None = None, inv_freq_name: str | None = None) -> None:
+        if pos_idx_name is not None:
+            self.pos_idx_name = pos_idx_name
+        else:
+            self.pos_idx_name = "pos_idx"
+
+        if inv_freq_name is not None:
+            self.inv_freq_name = inv_freq_name
+        else:
+            self.inv_freq_name = "inv_freq"
+
+    def declares(self) -> tuple[EpiOp, ...]:
+        return (ColVecLoad(self.pos_idx_name), RowVecLoad(self.inv_freq_name))
+
+    @cute.jit
+    def visit(
+        self,
+        gemm: GemmSm90,
+        params: ParamsBase,
+        epi_loop_tensors: dict,
+        tRS_rD: cute.Tensor,
+        tRS_rC: cute.Tensor | None,
+    ) -> tuple[cute.Tensor, ...]:
+        pos_idx = epi_loop_tensors.get(self.pos_idx_name)
+        inv_freq = epi_loop_tensors.get(self.inv_freq_name)
+        if cutlass.const_expr(pos_idx is not None and inv_freq is not None):
+            for i in cutlass.range_constexpr(cute.size(tRS_rD) // 2):
+                a = pos_idx[2 * i].to(dtype=gemm.acc_dtype) * inv_freq[2 * i].to(dtype=gemm.acc_dtype)
+                c = cute.math.cos(a)
+                s = cute.math.sin(a)
+                x = tRS_rD[2 * i]
+                y = tRS_rD[2 * i + 1]
+                tRS_rD[2 * i] = x * c + y * s
+                tRS_rD[2 * i + 1] = y * c - x * s
+        return ()
