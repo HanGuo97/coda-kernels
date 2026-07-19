9224f1cc2572fed77dc7002798b6ba61d49e41e8
Han Guo <alex.guohan1106@gmail.com>
2026-07-19 10:21:52 -0700

    added qknorm rope

---
+++ coda/kernels/functional/qknorm_rope.py
@@ -0,0 +1,29 @@
+import torch
+from fla.utils import autocast_custom_bwd, autocast_custom_fwd, input_guard
+
+from coda.core.elementwise.functional import qknorm_rope_fwd, qknorm_rope_bwd
+from coda.core.gemm.functional import _sqsum_num_segments, gemm, gemm_qkv_sqsum
+
+
+def linear_qknorm_rope(
+    x: torch.Tensor,
+    weight: torch.Tensor,
+    gamma: torch.Tensor,
+    positions: torch.Tensor,
+    frequencies: torch.Tensor,
+    num_heads_q: int,
+    num_heads_k: int,
+    head_dim: int,
+    eps: float,
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    return LinearQKNormRoPE.apply(
+        x,
+        weight,
+        gamma,
+        positions,
+        frequencies,
+        num_heads_q,
+        num_heads_k,
+        head_dim,
+        eps,
+    )
