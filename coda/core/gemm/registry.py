9a94e6ebbc720fba2c4af57dd61b7b4225dbff96
Han Guo <alex.guohan1106@gmail.com>
2026-07-24 10:08:21 -0700

    more epilogues

--- coda/core/gemm/registry.py
+++ coda/core/gemm/registry.py
@@ -2,10 +2,21 @@ from quack.gemm_sm90 import GemmSm90
 from quack.activation import gate_fn_map
 
 from coda.core.epilogue.base import compose
+from coda.core.epilogue.affine import Affine, Scale
 from coda.core.epilogue.activation import Gated, RoPE
 from coda.core.epilogue.lse import LSE, SelectLogits
 from coda.core.epilogue.qknorm import SqSum
 
+
+GemmScale = (
+    Scale(
+        auxiliary_store=False,
+    )
+    .bind(
+        name="GemmScale",
+        gemm_cls=GemmSm90,
+    )
+)
 
 GemmSwiGLU = (
     Gated(
@@ -17,10 +28,56 @@ GemmSwiGLU = (
     )
 )
 
+GemmScaleSwiGLU = (
+    compose(
+        [
+            Scale(
+                auxiliary_store=False,
+            ),
+            Gated(
+                fn=gate_fn_map["swiglu"],
+            ),
+        ]
+    )
+    .bind(
+        name="GemmScaleSwiGLU",
+        gemm_cls=GemmSm90,
+    )
+)
+
 GemmRoPE = (
-    RoPE()
+    RoPE(
+        auxiliary_store=False,
+    )
     .bind(
         name="GemmRoPE",
+        gemm_cls=GemmSm90,
+    )
+)
+
+GemmRoPEAux = (
+    RoPE(
+        auxiliary_store=True,
+    )
+    .bind(
+        name="GemmRoPEAux",
+        gemm_cls=GemmSm90,
+    )
+)
+
+GemmScaleRoPEAux = (
+    compose(
+        [
+            Scale(
+                auxiliary_store=False,
+            ),
+            RoPE(
+                auxiliary_store=True,
+            ),
+        ]
+    )
+    .bind(
+        name="GemmScaleRoPEAux",
         gemm_cls=GemmSm90,
     )
 )
@@ -42,6 +99,22 @@ GemmLSESelectLogits = (
     )
     .bind(
         name="GemmLSESelectLogits",
+        gemm_cls=GemmSm90,
+    )
+)
+
+GemmScaleLSESelectLogits = (
+    compose(
+        [
+            Scale(
+                auxiliary_store=False,
+            ),
+            LSE(),
+            SelectLogits(),
+        ]
+    )
+    .bind(
+        name="GemmScaleLSESelectLogits",
         gemm_cls=GemmSm90,
     )
 )
