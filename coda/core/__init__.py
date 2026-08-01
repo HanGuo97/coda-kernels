0d22f71e857b7359b52a6da2ef656e652d03561b
Han Guo <alex.guohan1106@gmail.com>
2026-08-01 14:58:46 -0700

    minor

--- coda/core/__init__.py
+++ coda/core/__init__.py
@@ -1,7 +1,9 @@
-from pathlib import Path
-
+import torch
 import quack.cache
+from pathlib import Path
 
 _CODA_CORE = Path(__file__).resolve().parent
 if _CODA_CORE not in quack.cache.EXTRA_SOURCE_DIRS:
     quack.cache.EXTRA_SOURCE_DIRS.append(_CODA_CORE)
+
+torch._dynamo.config.recompile_limit = max(torch._dynamo.config.recompile_limit, 128)
