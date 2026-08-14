5e93168f0304f32605ffaadba247c82b7b0265a7
Han Guo <alex.guohan1106@gmail.com>
2026-08-13 23:31:09 -0700

    type annotations

--- coda/core/epilogue/activations.py
+++ coda/core/epilogue/activations.py
@@ -1,10 +1,13 @@
 import cutlass.cute as cute
 from quack.activation import dswiglu, swiglu
-from quack.epilogue.math import pack, unpack
+from quack.epilogue.math import F2, Pair, pack, unpack
 from quack.epilogue.frontend import gemm_epilogue
+
+EpiValue = cute.Float32 | Pair | F2
+EpiOut = dict[str, EpiValue | tuple[EpiValue, EpiValue]]
 
 
 @gemm_epilogue(outputs=("postact",), mode="acc_pair")
-def swiglu_preact_epi(acc):
+def swiglu_preact_epi(acc: EpiValue) -> EpiOut:
     gate, up = unpack(acc)
     return {"D": acc, "postact": swiglu(gate, up)}
