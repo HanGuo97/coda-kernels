71307d1070a5338191012a210c06abd8c2eddd74
Han Guo <alex.guohan1106@gmail.com>
2026-08-15 00:36:48 -0700

    updated some activations

--- coda/core/epilogue/activations.py
+++ coda/core/epilogue/activations.py
@@ -2,6 +2,9 @@ import cutlass.cute as cute
 from quack.activation import dswiglu, swiglu
 from quack.epilogue.math import F2, Pair, pack, unpack
 from quack.epilogue.frontend import gemm_epilogue
+from quack.epilogue.ops import (
+    ColVecLoad,
+)
 
 EpiValue = cute.Float32 | Pair | F2
 EpiOut = dict[str, EpiValue | tuple[EpiValue, EpiValue]]
@@ -11,3 +14,13 @@ EpiOut = dict[str, EpiValue | tuple[EpiValue, EpiValue]]
 def swiglu_preact_epi(acc: EpiValue) -> EpiOut:
     gate, up = unpack(acc)
     return {"D": acc, "postact": swiglu(gate, up)}
+
+
+@gemm_epilogue(ops={"rstd": ColVecLoad("rstd")})
+def rstd_epi(acc: EpiValue, rstd: EpiValue) -> EpiOut:
+    return {"D": acc * rstd}
+
+
+@gemm_epilogue()
+def alpha_epi(acc: EpiValue, alpha: EpiValue) -> EpiOut:
+    return {"D": acc * alpha}
